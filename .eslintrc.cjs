69e7315dc82a268227311f27150c875823c10b0b
Isidro Gonzalez <igonzalezprs@gmail.com>
2024-05-28 20:15:31 -0400

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+
+  parserOptions: {
+    sourceType: "module",
+  },
+
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+};
